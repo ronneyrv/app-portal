657a1ac04b3b22c86b6b4a6263ae22a72488d830
Ronney Rocha <ronneyrv@gmail.com>
2025-04-17 21:52:43 -0300

    feature: add logomarca in the sidebar

--- src/components/SidebarDrawer.jsx
+++ src/components/SidebarDrawer.jsx
@@ -1,78 +1,83 @@
-import React from 'react';
-import {
-  Drawer as MuiDrawer,
-  IconButton,
-  Divider,
-} from '@mui/material';
-import { ChevronLeft, ChevronRight, Inbox, Mail } from '@mui/icons-material';
-import { styled } from '@mui/material/styles';
-import SidebarItems from './SidebarItems';
-import DirectionsBoatOutlinedIcon from '@mui/icons-material/DirectionsBoatOutlined';
-import AgricultureOutlinedIcon from '@mui/icons-material/AgricultureOutlined';
+import React from "react";
+import { Drawer as MuiDrawer, IconButton, Divider } from "@mui/material";
+import { ChevronLeft, ChevronRight, Inbox, Mail } from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+import SidebarItems from "./SidebarItems";
+import DirectionsBoatOutlinedIcon from "@mui/icons-material/DirectionsBoatOutlined";
+import AgricultureOutlinedIcon from "@mui/icons-material/AgricultureOutlined";
+import logo from "../assets/images/logo_pptm.png";
 
 const drawerWidth = 240;
 
 const drawerItems = [
-  { title: 'Descarregamento', icon: <DirectionsBoatOutlinedIcon />, path: '/descarregamento' },
-  { title: 'Horímetro', icon: <AgricultureOutlinedIcon />, path: '/horimetro' },
-]
+  {
+    title: "Descarregamento",
+    icon: <DirectionsBoatOutlinedIcon />,
+    path: "/descarregamento",
+  },
+  { title: "Horímetro", icon: <AgricultureOutlinedIcon />, path: "/horimetro" },
+];
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `60px`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `60px`,
   },
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
   ...(open && {
     ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
   }),
   ...(!open && {
     ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
   }),
 }));
 
 const SidebarDrawer = ({ open, toggleDrawer }) => {
-
   return (
     <Drawer variant="permanent" open={open}>
       <DrawerHeader>
+        <img
+          src={logo}
+          alt="Logo PPTM"
+          style={{ height: 40, objectFit: "contain" }}
+        />
+
         <IconButton onClick={toggleDrawer}>
           {open ? <ChevronLeft /> : <ChevronRight />}
         </IconButton>
       </DrawerHeader>
-      <Divider />
       <SidebarItems items={drawerItems} open={open} />
     </Drawer>
   );
