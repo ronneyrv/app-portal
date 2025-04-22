c2030726105261c15e1a7d318a673faf2df16950
Ronney Rocha <ronneyrv@gmail.com>
2025-04-22 12:05:54 -0300

    feature: infoTCLD componentization

---
+++ src/assets/config/DataDecarregamento.js
@@ -0,0 +1,15 @@
+const descarregamento = [
+  {
+    cliente: "ENERGIA PECEM",
+    navio: "MV TESTE MOCADO",
+    arqueacao: "75.456,25",
+    atracacao: "19/04/2025 10:30",
+    inicioOP: "19/04/2025 13:15",
+    saldo: "32.159,2",
+    fimOP: "22/04/2025 6:40",
+    meta: 3.5,
+    dias: 3.8,
+  },
+];
+
+export default descarregamento;
---
+++ src/components/InfoTCLD.jsx
@@ -0,0 +1,76 @@
+import Relogio from "../components/GraficRelogio";
+import '../styles/infotcld.css';
+
+export default function InfoTCLD({dados}) {
+    return(
+        <div className="grid-tcld">
+            <div className="info-navio">
+              <div className="info-navio-row">
+                <div>
+                  <label>Cliente:</label>
+                  <input
+                    type="text"
+                    style={{ width: "150px" }}
+                    defaultValue={dados[0].cliente}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label>Navio:</label>
+                  <input
+                    type="text"
+                    style={{ width: "230px" }}
+                    defaultValue={dados[0].navio}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label>Arqueação Inicial:</label>
+                  <input
+                    type="text"
+                    style={{ width: "120px" }}
+                    defaultValue={dados[0].arqueacao}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div className="info-navio-row">
+                <div>
+                  <label>Atracação:</label>
+                  <input
+                    type="text"
+                    defaultValue={dados[0].atracacao}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label>Início da operação:</label>
+                  <input
+                    type="text"
+                    defaultValue={dados[0].inicioOP}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div className="info-navio-row">
+                <div>
+                  <label>Saldo à Bordo:</label>
+                  <input type="text" defaultValue={dados[0].saldo} readOnly />
+                </div>
+                <div>
+                  <label>Previsão de Término:</label>
+                  <input type="text" defaultValue={dados[0].fimOP} readOnly />
+                </div>
+              </div>
+            </div>
+
+            <div className="info-relogio">
+              <Relogio
+                plano={dados[0].meta}
+                real={dados[0].dias}
+                readOnly
+              />
+            </div>
+          </div>
+    )
+}
