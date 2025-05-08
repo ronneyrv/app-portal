66bf9becfc0a7ea7e4015dd65e19439c82df2941
Ronney Rocha <ronneyrv@gmail.com>
2025-05-08 16:32:00 -0300

    feature: add component observacaoRot

--- src/components/Patio.jsx
+++ src/components/Patio.jsx
@@ -3,9 +3,6 @@ import SignalWifiStatusbar4BarIcon from "@mui/icons-material/SignalWifiStatusbar
 import SignalWifiOffIcon from "@mui/icons-material/SignalWifiOff";
 import SignalWifiConnectedNoInternet4Icon from "@mui/icons-material/SignalWifiConnectedNoInternet4";
 import Divider from "@mui/material/Divider";
-import WaterDropIcon from "@mui/icons-material/WaterDrop";
-import GppGoodIcon from "@mui/icons-material/GppGood";
-import GppBadIcon from "@mui/icons-material/GppBad";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
@@ -20,9 +17,6 @@ import {
 import NotifyBar from "../components/NotifyBar";
 
 import alturas from "../assets/config/DataAlturas";
-import polimero from "../assets/config/DataPolimero";
-import stqPolimero from "../assets/config/DataStdPolimero";
-import sisUmectacao from "../assets/config/DataSisUmectacao";
 
 import "../styles/patio.css";
 
@@ -37,7 +31,9 @@ export default function Patio() {
     severity: "success",
   });
 
-  useEffect(() => {fetchCanhoes()}, []);
+  useEffect(() => {
+    fetchCanhoes();
+  }, []);
 
   const iconMap = {
     disponivel: (
@@ -222,226 +218,145 @@ export default function Patio() {
         </Box>
       </Modal>
 
-      <div className="containe-patio">
-        <div className=" layout">
-          <div className="patio">
-            <div className="patio-3">
-              <div className="can-linha-6">
-                {[...canhoes.slice(45, 54).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="pilhas">
-                <div className="pilha-3B">{renderPilha("3B")}</div>
-                <div className="pilha-3A">{renderPilha("3A")}</div>
-              </div>
-              <div className="can-linha-5">
-                {[...canhoes.slice(36, 45).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <Divider />
-            <div className="patio-2">
-              <div className="can-linha-4">
-                {[...canhoes.slice(27, 36).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="pilhas">
-                <div className="pilha-2D">{renderPilha("2D")}</div>
-                <div className="pilha-2C">{renderPilha("2C")}</div>
-                <div className="pilha-2B">{renderPilha("2B")}</div>
-                <div className="pilha-2A">{renderPilha("2A")}</div>
-              </div>
-              <div className="can-linha-3">
-                {[...canhoes.slice(18, 27).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <Divider />
-            <div className="patio-1">
-              <div className="can-linha-2">
-                {[...canhoes.slice(9, 18).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-
-              <div className="pilhas">
-                <div className="pilha-1B">{renderPilha("1B")}</div>
-                <div className="pilha-1A">{renderPilha("1A")}</div>
-              </div>
-
-              <div className="can-linha-1">
-                {[...canhoes.slice(0, 9).reverse()].map((item, index) => {
-                  const IconComponent = iconMap[item.modo];
-                  const rotation = rotationMap[item.posicao] || "0deg";
-                  return (
-                    <div key={index} title={item.can}>
-                      <span
-                        style={{
-                          display: "inline-block",
-                          transform: `rotate(${rotation})`,
-                        }}
-                        onClick={() => configCanhao(item.can, item.modo)}
-                      >
-                        {IconComponent}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="observacao">
-        <div className="umectacao">
-            <h3>Sistema de Umectação</h3>
-            <div>
-              {sisUmectacao.map((item, index) => (
-                <div key={index} className="umectacao-status">
-                  {item.disponível ? (
-                    <GppGoodIcon style={{ fontSize: 20, color: "#76ff03" }} />
-                  ) : (
-                    <GppBadIcon style={{ fontSize: 20, color: "#ff1744" }} />
-                  )}
-                  {item.disponível ? (
-                    <h4>Sistema Disponível</h4>
-                  ) : (
-                    <h4>Sistema Indisponível</h4>
-                  )}
-                </div>
-              ))}
-
-              <div className="umectacao-legenda">
-                <label>Legenda:</label>
-                <div className="legenda">
-                  <div>
-                    <SignalWifiStatusbar4BarIcon
-                      style={{ fontSize: 15, color: "#76ff03" }}
-                    />
-                    <span>DISPONÍVEL</span>
+      <div className=" layout">
+        <div className="patio">
+          <div className="patio-3">
+            <div className="can-linha-6">
+              {[...canhoes.slice(45, 54).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
                   </div>
-                  <div>
-                    <SignalWifiOffIcon
-                      style={{ fontSize: 15, color: "#757575" }}
-                    />
-                    <span>DESABILITADO</span>
+                );
+              })}
+            </div>
+            <div className="pilhas">
+              <div className="pilha-3B">{renderPilha("3B")}</div>
+              <div className="pilha-3A">{renderPilha("3A")}</div>
+            </div>
+            <div className="can-linha-5">
+              {[...canhoes.slice(36, 45).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
                   </div>
-                  <div>
-                    <SignalWifiConnectedNoInternet4Icon
-                      style={{ fontSize: 15, color: "#FF0000" }}
-                    />
-                    <span>INDISPONÍVEL</span>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </div>
           <Divider />
-
-          <div className="polimero">
-            <h3>Validade do Polímero</h3>
-            {polimero.map((item) => (
-              <div
-                key={item.pilha}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "3px",
-                }}
-              >
-                <WaterDropIcon style={{ fontSize: 15, color: item.cor }} />
-                <span>
-                  {item.pilha}: {item.validade} - {item.acao}
-                </span>
-              </div>
-            ))}
-            <label>Estoque de Polímero:</label>
-            <div className="stqPol">
-              {stqPolimero.map((item, index) => (
-                <div key={index}>
-                  {item.cliente}: {item.litro} litros
-                </div>
-              ))}
+          <div className="patio-2">
+            <div className="can-linha-4">
+              {[...canhoes.slice(27, 36).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="pilhas">
+              <div className="pilha-2D">{renderPilha("2D")}</div>
+              <div className="pilha-2C">{renderPilha("2C")}</div>
+              <div className="pilha-2B">{renderPilha("2B")}</div>
+              <div className="pilha-2A">{renderPilha("2A")}</div>
+            </div>
+            <div className="can-linha-3">
+              {[...canhoes.slice(18, 27).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <Divider />
+          <div className="patio-1">
+            <div className="can-linha-2">
+              {[...canhoes.slice(9, 18).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
 
-          <div className="obs">
-            <h3>Observações:</h3>
-            <textarea rows={7}></textarea>
+            <div className="pilhas">
+              <div className="pilha-1B">{renderPilha("1B")}</div>
+              <div className="pilha-1A">{renderPilha("1A")}</div>
+            </div>
+
+            <div className="can-linha-1">
+              {[...canhoes.slice(0, 9).reverse()].map((item, index) => {
+                const IconComponent = iconMap[item.modo];
+                const rotation = rotationMap[item.posicao] || "0deg";
+                return (
+                  <div key={index} title={item.can}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        transform: `rotate(${rotation})`,
+                      }}
+                      onClick={() => configCanhao(item.can, item.modo)}
+                    >
+                      {IconComponent}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/PatioObservacao.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import NotifyBar from "../components/NotifyBar";
+import "../styles/patioobservacao.css";
+
+export default function PatioObs() {
+  const [obs, setObs] = useState([]);
+
+  const [notify, setNotify] = useState({
+    open: false,
+    message: "",
+    severity: "success",
+  });
+
+  useEffect(() => {
+    fetchBuscarObs();
+  }, []);
+
+  const fetchBuscarObs = () => {
+    fetch("http://localhost:3001/observacao/rot", {
+      credentials: "include",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.error("HTTP status:", res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.type === "success") {
+          setObs(data.data[0].observacao);
+        } else {
+          console.error("Erro ao buscar observações");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro de rede:", error);
+      });
+  };
+
+  const fetchAtualizarObs = (text) => {
+    fetch("http://localhost:3001/observacao/rot/set", {
+      credentials: "include",
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newObs: text }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.error("HTTP status:", res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.type === "success") {
+          fetchBuscarObs();
+        } else {
+          console.log(data.message);
+          fetchBuscarObs();
+        }
+      })
+      .catch((err) => {
+        console.error("Erro na base de dados:", err);
+        fetchBuscarObs();
+      });
+  };
+
+  return (
+    <>
+      <NotifyBar
+        open={notify.open}
+        message={notify.message}
+        severity={notify.severity}
+        onClose={() => setNotify({ ...notify, open: false })}
+      />
+      <div className="obs">
+        <h3>Observações:</h3>
+        <textarea
+          rows={7}
+          value={obs}
+          onChange={(e) => setObs(e.target.value)}
+          onBlur={(e) => fetchAtualizarObs(e.target.value)}
+        />
+      </div>
+    </>
+  );
+}
