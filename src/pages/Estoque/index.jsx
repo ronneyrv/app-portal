9d168bfc8671a6646d201889804ef21cb6b1d3ed
Ronney Rocha <ronneyrv@gmail.com>
2025-07-25 14:15:25 -0300

    feature: add estoque page

--- src/pages/Estoque/index.jsx
+++ src/pages/Estoque/index.jsx
@@ -1,11 +1,19 @@
 import "./estoque.css";
 import PizzaEstoque from "../../components/Estoque/PizzaEstoque";
+import LinhasEstoque from "../../components/Estoque/LinhasEstoque";
+import TabelaEstoque from "../../components/Estoque/tabelaEstoque";
 
 export default function Estoque() {
   return (
     <div className="main-estoque">
       <h3>Estoque de Carvão</h3>
-      <PizzaEstoque />
+      <div className="container">
+        <PizzaEstoque />
+        <div className="linhas-container">
+          <LinhasEstoque />
+        </div>
+      </div>
+      <TabelaEstoque />
     </div>
   );
 }
